1aab50537aaa143839d08275e3076ffd60b75e85
Thomas Levine <occurrence@thomaslevine.com>
2013-04-12 14:42:17 -0400

    retry

--- heroku.sh
+++ heroku.sh
@@ -12,22 +12,29 @@ for dependency in xmlstarlet jshon; do
   fi
 done
 
-# Set app seed.
-app_seed=$RANDOM
-
-# Global variables
-APPGEN_URL="http://appgen.herokuapp.com"
-tmp="/tmp/$app_seed"
-
-# Choose an app name
-wget -O "$tmp" "${APPGEN_URL}?seed=${app_seed}"
-app_name=$(grep h1 "$tmp" | xmlstarlet sel -t -v '//h1'| tr -d \  | tr [A-Z] [a-z])
-
-# Make the app
-curl -H "Accept: application/json" \
-     -u ":$HEROKU_API_KEY" \
-     -d "app[name]=$app_name" \
-     -X POST https://api.heroku.com/apps > "$tmp"
+new_app() {
+  # Set app seed.
+  app_seed=$RANDOM
+  
+  # Global variables
+  APPGEN_URL="http://appgen.herokuapp.com"
+  tmp="/tmp/$app_seed"
+  
+  # Choose an app name
+  wget -O "$tmp" "${APPGEN_URL}?seed=${app_seed}"
+  app_name=$(grep h1 "$tmp" | xmlstarlet sel -t -v '//h1'| tr -d \  | tr [A-Z] [a-z])
+  
+  # Make the app
+  curl -H "Accept: application/json" \
+       -u ":$HEROKU_API_KEY" \
+       -d "app[name]=$app_name" \
+       -X POST https://api.heroku.com/apps > "$tmp"
+  
+  # Retry if the name is already taken.
+  if cat "$tmp" | grep '"error":"Name is already taken"'; then
+    new_app
+  fi
+} 
 
 # Push the Ruby proxy to  the app
 git_url=$(cat "$tmp" | jshon -e git_url -u)
